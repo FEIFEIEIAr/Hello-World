9ef107cf0fcb4644a70fc23fbcb204d6ca8868f7
FEIFEIEIAr <77225830+FEIFEIEIAr@users.noreply.github.com>
2021-07-14 15:35:43 +0800

    Create 10_monkey.py

---
+++ Deep Learning/nerual network/CNN/10_monkey.py
@@ -0,0 +1,12 @@
+%matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import sklearn
+import sys
+import tensorflow as tf
+from tensorflow import keras
+import random
+
